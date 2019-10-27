83ec1b871abdfacfacba55f81c8082337ecbb856
Anwesh43 <anweshthecool0@gmail.com>
2019-10-27 14:51:19 +0530

    adding LBBNode class

--- index.ts
+++ index.ts
@@ -141,3 +141,49 @@ class Animator {
         }
     }
 }
+
+class LBBNode {
+
+    prev : LBBNode
+    next : LBBNode
+    state : State = new State()
+
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LBBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLCCNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LBBNode {
+        var curr : LBBNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
